315efb89fa31cf62ef4d45d5c1c1bcf1b9cd574f
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-03 05:13:00 -0700

    challenge 3

--- python_functions.py
+++ python_functions.py
@@ -8,3 +8,27 @@ def sum_to(n):
 
 print(sum_to(10))
 '''
+
+
+
+#Challenge 2
+'''
+def largest(list):
+  biggest = 0
+  for item in list:
+    if item > biggest:
+      biggest = item
+  return biggest
+
+print(largest([10, 4, 2, 231, 91, 534]))
+'''
+
+
+#Challenge 3
+'''
+'''
+def occurrences(str1, str2):
+  list = str1.split(str2)
+  return len(list) - 1
+
+print(occurrences('hello, lol. la lala.', 'l'))
