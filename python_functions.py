28165555cf97690dcfcfe96605a76e735ea540cc
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-03 05:16:54 -0700

    challenge 4

--- python_functions.py
+++ python_functions.py
@@ -26,9 +26,20 @@ print(largest([10, 4, 2, 231, 91, 534]))
 
 #Challenge 3
 '''
-'''
 def occurrences(str1, str2):
   list = str1.split(str2)
   return len(list) - 1
 
 print(occurrences('hello, lol. la lala.', 'l'))
+'''
+
+
+
+#Challenge 4
+def product(*args):
+  result = 1
+  for arg in args:
+    result *= arg
+  return result
+
+print(product(2, 3, 4, 7, 4, 3, 8, 5))
