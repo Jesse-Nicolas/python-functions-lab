52282a5816c9fd3ad69e904d42d96197d5d5de70
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-03 05:17:49 -0700

    complete

--- python_functions.py
+++ python_functions.py
@@ -1,5 +1,4 @@
 #Challenge 1
-'''
 def sum_to(n):
   total = 0
   for num in range(n+1):
@@ -7,12 +6,10 @@ def sum_to(n):
   return total
 
 print(sum_to(10))
-'''
 
 
 
 #Challenge 2
-'''
 def largest(list):
   biggest = 0
   for item in list:
@@ -21,17 +18,14 @@ def largest(list):
   return biggest
 
 print(largest([10, 4, 2, 231, 91, 534]))
-'''
 
 
 #Challenge 3
-'''
 def occurrences(str1, str2):
   list = str1.split(str2)
   return len(list) - 1
 
 print(occurrences('hello, lol. la lala.', 'l'))
-'''
 
 
 
